26141e1130d0352e715b769a8e95e40f6e20aa40
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-10-02 22:58:43 +0700

    Layouting and routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+Route::get('add', [AdminController::class, 'create'])->name('admin.create');
+Route::post('store', [AdminController::class, 'store'])->name('admin.store');
+Route::get('edit/{id}', [AdminController::class, 'edit'])->name('admin.edit');
+Route::post('update/{id}', [AdminController::class, 'update'])->name('admin.update');
+Route::post('delete/{id}', [AdminController::class, 'delete'])->name('admin.delete');
